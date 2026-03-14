2553aba9ff761c3d4f4b42fc4513f2d577b9db51
dushyant kumar <dushyantvelar@gmail.com>
2026-03-14 14:37:36 +0530

    initial setup with db & server

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import  cors from "cors";
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/hello",(req,res)=>{
+    res.json("hello");
+});
+
+
+export default app;
